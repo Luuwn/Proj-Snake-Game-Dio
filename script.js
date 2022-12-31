0d4a7abb1533bb4610b71b0d81d4d2ba551134d8
Luan Costa <luan.coast@outlook.com.br>
2022-12-31 16:56:07 -0300

    Random spawn da comida

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ let canvas = document.getElementById('snake'),
 
 let snake = [];
 snake[0] = {
-    x: 7 * box,
-    y: 7 * box
+    x: 8 * box,
+    y: 8 * box
 }
 
 let direction = "right";
@@ -28,7 +28,7 @@ function criarCobrinha(){
 }
 
 function drawFood(){
-    context.fillStyle = "red";
+    context.fillStyle = "brown";
     context.fillRect(food.x, food.y, box, box);
 }
 
@@ -43,9 +43,9 @@ function update (event){
 
 function iniciarJogo(){
 
-    if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+    if(snake[0].x > 16 * box && direction == "right") snake[0].x = 0;
     if(snake[0].x < 0  && direction == "left") snake[0].x = 16 * box;
-    if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+    if(snake[0].y > 16 * box && direction == "down") snake[0].y = 0;
     if(snake[0].y < 0  && direction == "up") snake[0].y = 16 * box;
 
     criarBG();
@@ -60,7 +60,13 @@ function iniciarJogo(){
     if(direction == "up") snakeY -= box;
     if(direction == "down") snakeY += box;
 
-    snake.pop();
+    if(snakeX != food.x || snakeY != food.y){
+        snake.pop();
+    } 
+    else{
+        food.x = Math.floor(Math.random() * 15 + 1) * box;
+        food.y = Math.floor(Math.random() * 15 + 1) * box;
+    }
 
     let newHead = {
         x: snakeX,
